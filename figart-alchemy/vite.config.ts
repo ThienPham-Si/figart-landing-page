18bb5b160e17d0170c77c531e8a5c550f87f7562
ThienPham-Si <82398747+ThienPham-Si@users.noreply.github.com>
2025-05-04 23:14:07 -0700

    update config

--- figart-alchemy/vite.config.ts
+++ figart-alchemy/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'node:path'
+import * as fs from 'node:fs'
 
-// https://vite.dev/config/
+// Get all HTML files in the root directory
+const htmlFiles: string[] = fs.readdirSync('.').filter((file: string) => file.endsWith('.html'))
+const input: Record<string, string> = {}
+htmlFiles.forEach((file: string) => {
+  input[file.replace('.html', '')] = path.resolve(process.cwd(), file)
+})
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input
+    }
+  }
 })
